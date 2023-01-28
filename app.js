0d8db6dea7b551f0a3c8c2d19d62a43418e0024d
Yulia Likhytska <Yulia.Likhytska@contracted.pmi.com>
2023-01-28 18:48:35 +0100

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "./build")));
+// app.get("/", function (req, res, next) {
+//   res.render("public/index.html");
+// });
+app.get("/", async (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../build"), "index.html");
+});
+
+app.listen(8080, "0.0.0.0", (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server running on post: ", 8080);
+});
--- src/client/Activity.jsx
+++ src/client/Activity.jsx
@@ -1,26 +1,24 @@
-
 import React, { Component } from "react";
-import Postmonger from 'postmonger';
-import postmonger from "postmonger";
+import Postmonger from "postmonger";
 var connection = new Postmonger.Session();
 
 export class Activity extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data:{}
-    }
+      data: {},
+    };
   }
-  componentDidMount(){
-    connection.on('initActivity',(data) => this.setState({payload:data}));
-    connection.trigger('ready');
-    console.log('in componentDIdMount');
+  componentDidMount() {
+    connection.on("initActivity", (data) => this.setState({ payload: data }));
+    connection.trigger("ready");
+    console.log("in componentDIdMount");
     // connection.trigger('updateButton',{
     //     button
     // })
   }
-    render() {
-      return <div>Hello from  aa </div>;
-    }
+  render() {
+    return <div>Hello from aa </div>;
   }
+}
 export default Activity;
