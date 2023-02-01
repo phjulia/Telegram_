ab7fc7c0af767469e1df11ab4efaf4e3dc355439
Yulia Likhytska <Yulia.Likhytska@contracted.pmi.com>
2023-02-01 18:07:55 +0100

    Config

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.get("/config.json", (req, res) => {
-  res.sendFile("./config.json");
+  res.status(200).sendFile(__dirname + "/config.json");
+  console.log("Sent file config");
+  console.log(__dirname);
 });
 module.exports = router;
