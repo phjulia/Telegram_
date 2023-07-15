a16f94f909cf45587a42f51dc03518d7a561f1dd
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-15 23:24:19 +0200

    6

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ fastify.register(require("@fastify/session"), {
 });
 // app.register(require("./src/server/api"));
 // fastify.register(require("./src/server/api.js"));
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/public/",
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "dist"),
 });
 console.log(" path.join(__dirname, 'public')", path.join(__dirname, "public"));
 fastify.register(require("./src/server/routes.js"));
