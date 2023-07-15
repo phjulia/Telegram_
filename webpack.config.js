78406cfcf2e366266f25d4a2eba22cde546c85f4
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-15 23:41:03 +0200

    6

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/client/index.jsx",
+  entry: "../src/client/index.jsx",
   module: {
     // exclude node_modules
     rules: [
