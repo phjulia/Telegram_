1284b16204aaf0a71ee6f26827d4c30eb66ae67b
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-16 12:44:58 +0200

    1

--- src/client/Activity.jsx
+++ src/client/Activity.jsx
@@ -3,10 +3,14 @@ import Step1 from "./steps/Step1.jsx";
 import esd from "../client/components/entrySourceDispatcher.js";
 import StepManager from "../client/StepManager.jsx";
 
+const connection = new Postmonger.Session();
 export class Activity extends Component {
   constructor(props) {
     super(props);
     this.esd = esd.bind(this);
+  }
+  componentDidMount() {
+    connection.trigger("ready"); //called any time there is a load betwene JB and Cust App(on IFrame load)
   }
   render() {
     return (
