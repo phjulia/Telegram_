4fdc664fd04eaee661cb47f8e8b67241d2b715b6
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-16 10:12:31 +0200

    1

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
 // import App from './App';
 import Activity from "./Activity.jsx";
+// import { Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log("in the index js");
 root.render(
   // <React.StrictMode>
   //   <Switch>
-  <Router>
-    <Route exact path="/telegram" component={Activity}></Route>
-  </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/telegram" component={Activity}></Route>
+    </Routes>
+  </BrowserRouter>
   /* </Switch>
     <Activity />
   </React.StrictMode> */
