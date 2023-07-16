6a88931ed6a310cc551a5b233b11a37772a3a74d
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-07-16 12:21:39 +0200

    1

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
 // import App from './App';
