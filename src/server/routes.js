be2e47dd5d037d2e66c66e768e131eacc5ad7a12
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-08-18 13:20:00 +0200

    test

--- src/server/routes.js
+++ src/server/routes.js
@@ -56,10 +56,6 @@ module.exports = function (fastify, options, next) {
     res.redirect("https://telegram-mn8c.onrender.com/");
   });
   fastify.get("/", (req, res) => {
-    console.log(path.join(__dirname, "client/index.html"));
-    console.log(__dirname);
-    console.log(path.join(__dirname, "../client"));
-    console.log(path.join(path.resolve("src", "client", "index.html")));
     res.sendFile(path.join(__dirname, "index.html"));
   });
   fastify.get("/telegram.png", (req, res) => {
