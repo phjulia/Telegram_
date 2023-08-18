855441259c2ec20df3c6a3d4c750dca7d7bd4254
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-08-18 15:51:53 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 //const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/client/index.jsx"],
+  entry: ["./src/client/index.js"],
   output: {
     path: path.join(__dirname, "/build"),
     //path: path.join(__dirname, "public/activity/edit"),
