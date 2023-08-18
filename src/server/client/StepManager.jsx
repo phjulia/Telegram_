e3546761ac176a9a53986abfaa7fd20ef7d51204
Yuliia Likhytska <yuliia.likhytska@gmail.com>
2023-08-18 12:54:06 +0200

    test

---
+++ src/server/client/StepManager.jsx
@@ -0,0 +1,158 @@
+import React, { Component } from "react";
+import Postmonger from "postmonger";
+import PropTypes from "prop-types";
+import Step1 from "./steps/Step1.jsx";
+import handler from "../server/handler.js";
+var connection = new Postmonger.Session();
+export class StepManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      payload: {
+        metadata: {
+          isConfigured: false,
+        },
+        arguments: {
+          execute: {
+            inArguments: [
+              {
+                isConfigured: false,
+              },
+            ],
+          },
+        },
+      },
+      data: { message: "" },
+      config: {},
+    };
+    //this.buttonHandler = this.buttonHandler.bind(this);
+    this.saveState = this.saveState.bind(this);
+    this.readyHandler = this.readyHandler.bind(this);
+    this.handleDone = this.handleDone.bind(this);
+  }
+  saveState(m) {
+    this.setState({
+      data: {
+        message: m,
+      },
+    });
+  }
+  /**
+   * @description set state after the component has been updated
+   */
+  readyHandler() {
+    this.setState(
+      (prevState) => {},
+      () => connection.trigger("ready") //called any time there is a load betwene JB and Cust App(on IFrame load)
+    );
+  }
+  componentDidUpdate() {
+    this.readyHandler();
+  }
+  componentDidMount() {
+    //connection.on("initActivity", (data) => this.setState({ payload: data }));
+    //connection.on("clickedNext", (data) => this.handleDone());
+    //connection.on("clickedBack"..);
+    connection.trigger("ready");
+    /**----------------------------------------- */
+    connection.on("requestedTokens", (data) => this.setState({ tokens: data }));
+    connection.on("requestedCulture", (data) =>
+      this.setState({ culture: data })
+    );
+    connection.on("requestedEndpoints", (data) =>
+      this.setState({ endpoints: data })
+    );
+    connection.on("initActivity", (data) => {
+      if (data) {
+        this.setState({
+          payload: data,
+          type: "activity",
+        });
+      }
+      this.setState({
+        payload: {
+          metaData: {
+            isConfigured: false,
+          },
+          arguments: {
+            execute: {
+              inArguments: [],
+            },
+          },
+        },
+        type: "activity",
+      });
+    });
+    connection.on("initActivityRunningHover", (data) =>
+      this.setState({ payload: data, type: "activityHover" })
+    );
+    connection.on("initActivityRunningModal", (data) =>
+      this.setState({ payload: data, type: "activityModal" })
+    );
+    connection.on("requestedSchema", (data) =>
+      this.setState({ schema: data.schema })
+    );
+    //connection.on("clickedNext", (data) => this.setState({data:}));
+    connection.on("clickedNext", async (data) => {
+      console.log("clickedNext");
+      await this.handleDone(data);
+    });
+    connection.on("requestedInteraction", (data) =>
+      this.setState({ interaction: data })
+    );
+    connection.on("requestedTriggerEventDefinition", (data) =>
+      this.setState({ eventDefinition: data })
+    );
+    connection.trigger("ready");
+    connection.trigger("requestTokens");
+    connection.trigger("requestCulture");
+    connection.trigger("requestEndpoints");
+    connection.trigger("requestTriggerEventDefinition");
+    connection.trigger("requestSchema");
+    connection.trigger("requestInteraction");
+  }
+  async handleDone(data) {
+    console.log("Handle DONE");
+    this.setState(
+      (prevState) => {
+        prevState.payload.metaData.isConfigured = true;
+        prevState.payload.arguments.inArguments = [
+          { message: this.state.data.message },
+        ];
+        connection.trigger("updateActivity", prevState.payload);
+        return prevState;
+      },
+      () => connection.trigger("ready")
+    );
+    console.log(this.state.payload.arguments.inArguments);
+    await handler.sendOutboundMessage([1, "messgae"]);
+    //payload.metaData.isConfigured=true;
+
+    //connection.trigger("updateActivity",payload);
+    // this.setState(
+    //   (prevState) => {
+    //     prevState.payload.metaData.isConfigured = true;
+    //     prevState.payload = this.props.onSubmit(prevState);
+
+    //     // prevState.configured =
+    //     //   prevState.payload.arguments.execute.inArguments[0].configured;
+    //     connection.trigger("updateActivity", prevState.payload);
+    //   },
+    //   () => connection.trigger("ready")
+    // );
+  }
+  render() {
+    return (
+      <div
+        style={{ padding: "1.5rem 1rem 0px", background: "rgb(244, 246, 249)" }}
+      >
+        <Step1 saveState={this.saveState}></Step1>
+      </div>
+    );
+  }
+}
+StepManager.propTypes = {
+  children: PropTypes.object,
+  onSubmit: PropTypes.func,
+};
+export default StepManager;
---
+++ src/server/client/steps/Step1.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Textarea from "@salesforce/design-system-react/components/textarea";
+import PropTypes from "prop-types";
+import Icon from "@salesforce/design-system-react/components/icon";
+import { Card } from "@salesforce/design-system-react";
+import DataModelPicker from "../components/DataModelPicker";
+import Postmonger from "postmonger";
+var connection = new Postmonger.Session();
+
+class Step1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: {},
+      config: {},
+      error: {},
+    };
+    // this.buttonHandler = this.buttonHandler.bind(this);
+    //this.props.saveState=this.saveState.bind(this);
+    props.message = props.message || "";
+    this.handleChange = this.handleChange.bind(this);
+    //this.handleTextClick = this.handleTextClick.bind(this);
+  }
+  onClick() {}
+  handleChange(e) {
+    this.props.message = e.target.value;
+    this.props.saveState(e.target.value);
+    //this.props.configured = true;
+  }
+  /**
+   * @description set state after the component has been updated
+   */
+  readyHandler() {
+    connection.trigger("ready");
+  }
+  render() {
+    return (
+      <>
+        <section className="slds-grid slds-grid_vertical-align-center">
+          <div className="slds-col">
+            <div className="slds-grid slds-gutters">
+              <div className="slds-col slds-size_1-of-3">
+                <Card
+                  heading="Personalization"
+                  icon={<Icon category="action" name="user" size="small" />}
+                  bodyClassName="dataPickerCard"
+                ></Card>
+                <DataModelPicker journeyData={this.props.schema} />
+              </div>
+              <div className="slds-col slds-size_2-of-3">
+                <h1 className="slds-text-title_caps slds-p-vertical_medium">
+                  Add the text message:
+                </h1>
+                <Textarea
+                  required
+                  onChange={this.handleChange}
+                  // onClick={this.handleTextClick}
+                  value={this.props.message}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
+Step1.PropTypes = {
+  config: PropTypes.object,
+};
+export default Step1;
